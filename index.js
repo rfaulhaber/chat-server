dc818f58d9f1c94ad0180efa72e80b113f3eea00
Ryan Faulhaber <faulhaberryan@gmail.com>
2018-01-28 14:59:55 -0500

    removed mongo, added redis

--- index.js
+++ index.js
@@ -7,48 +7,44 @@ const { createStore } = require('redux');
 const reducer = require('./lib/reducer').default;
 const store = createStore(reducer);
 
-const MongoClient = require('mongodb').MongoClient;
-const mongodbURL = 'mongodb://localhost:27017';
+const redis = require('redis');
+const redisClient = redis.createClient();
 
-const dbname = 'chatdb';
+redisClient.on('error', function(error) {
+    console.log('error', error);
+});
 
-initializeDB().then(db => {
-    console.log('db initialized');
+const port = process.env.PORT || 3000;
 
-    const users = db.collection('users');
+io.on('connect', function(socket) {
+    console.log('new connection found');
 
-    const port = process.env.PORT || 3000;
+    const userID = shortid.generate();
+    console.log('generated unique id:', userID);
 
-    io.on('connect', function(socket) {
-        console.log('new connection found');
-
-        const userID = shortid.generate();
-        console.log('generated unique id:', userID);
-
-        users.insertOne({
-            id: userID
-        }).then(({insertedId}) => {
-            console.log('inserted id: ', insertedId);
-            socket.send({id: userID});
-        });
-
-        socket.on('disconnect', function() {
-            console.log('user disconnected');
-        });
-
-        socket.on('chat message', function(msg) {
-            console.log('message received', msg);
-            socket.broadcast.emit('chat message', msg);
-        });
-
-        socket.on('saveID', function(msg) {
-            console.log('save id msg', msg);
-        });
+    users.insertOne({
+        id: userID
+    }).then(({insertedId}) => {
+        console.log('inserted id: ', insertedId);
+        socket.send({id: userID});
     });
 
-    http.listen(port, function() {
-        console.log('listening on *:3000');
+    socket.on('disconnect', function() {
+        console.log('user disconnected');
     });
+
+    socket.on('chat message', function(msg) {
+        console.log('message received', msg);
+        socket.broadcast.emit('chat message', msg);
+    });
+
+    socket.on('saveID', function(msg) {
+        console.log('save id msg', msg);
+    });
+});
+
+http.listen(port, function() {
+    console.log('listening on *:3000');
 });
 
 function initializeDB() {
